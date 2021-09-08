672d38c4049195b10b8c956a74597fbe47f1091b
Andreas Kring <andreas@magenta.dk>
2021-09-08 10:51:15 +0200

    Fixed bug

--- os2mo_fastapi_utils/auth/test_helper.py
+++ os2mo_fastapi_utils/auth/test_helper.py
@@ -20,9 +20,13 @@ def ensure_endpoints_depend_on_oidc_auth_function(
     # auth coroutine.
 
     # Skip the starlette.routing.Route's (defined by the framework)
-    routes = filter(lambda _route: isinstance(route, APIRoute), all_routes)
+    routes = filter(lambda _route: isinstance(_route, APIRoute), all_routes)
     # Only check endpoints not in the NO_AUTH_ENDPOINTS list
-    routes = filter(lambda _route: route.path not in no_auth_endpoints, routes)
+    routes = filter(lambda _route: _route.path not in no_auth_endpoints, routes)
+
+    # Make sure that routes are defined
+    assert list(routes)
+
     for route in routes:
         has_auth = lookup_auth_dependency(route, auth_coro)
         assert has_auth, f"Route not protected: {route.path}"
@@ -31,7 +35,7 @@ def ensure_endpoints_depend_on_oidc_auth_function(
 def ensure_no_auth_endpoints_do_not_depend_on_auth_function(
     all_routes, no_auth_endpoints, auth_coro
 ):
-    no_auth_routes = filter(lambda _route: route.path in no_auth_endpoints, all_routes)
+    no_auth_routes = filter(lambda _route: _route.path in no_auth_endpoints, all_routes)
     for route in no_auth_routes:
         has_auth = lookup_auth_dependency(route, auth_coro)
         assert not has_auth, f"Route protected: {route.path}"
